365d6c5a5dcf332e42099706539ba6e9f03ad392
LieNautz <tuan_lai01@hotmail.com>
2022-09-11 19:59:35 +1000

    completed

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function (responseFromApi) {
+      return responseFromApi.json();
+    })
+    .then(function (jsonFormattedData) {
+      return renderBooks(jsonFormattedData);
+    });
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
